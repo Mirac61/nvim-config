07859d499de2cee7140c5ce35c001a3714ad55c0
mirac <mirac.i.sancak@gmail.com>
2026-08-06 16:07:33 +0200

    feat: keymaps and tools

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -8,4 +8,19 @@ return {
       open_for_directories = true,
     },
   },
+  {
+    "pwntester/octo.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    cmd = "Octo",
+    opts = {
+      picker = "snacks",
+      use_local_fs = true,
+    },
+    keys = {
+      { "<leader>gi", "<cmd>Octo issue list<cr>", desc = "GitHub Issues" },
+      { "<leader>gI", "<cmd>Octo issue create<cr>", desc = "GitHub Issue erstellen" },
+      { "<leader>gp", "<cmd>Octo pr list<cr>", desc = "GitHub PRs" },
+      { "<leader>gs", "<cmd>Octo search<cr>", desc = "GitHub Suche" },
+    },
+  },
 }
